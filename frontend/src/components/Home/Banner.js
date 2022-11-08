8d902200a27f8cc8fbd006aaa859717446e381df
Lital <litalbin24@gmail.com>
2022-11-08 15:29:40 +0200

    Show search box after clicking on "get"

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,15 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../imgs/logo.png";
 import SearchBox from "./SearchBox";
 
 const Banner = () => {
+  const [showSearchBox, setShowSearchBox] = useState(false);
+
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
         <img src={logo} alt="banner" />
         <div>
-          <span id="get-part">A place to get</span>
-          <SearchBox />
+          <span id="get-part">A place to </span><span onClick={() => {setShowSearchBox(true)}}>get</span>
+          {showSearchBox && <SearchBox />}
           <span> the cool stuff.</span>
         </div>
       </div>
